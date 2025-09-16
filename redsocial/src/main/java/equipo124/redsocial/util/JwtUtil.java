6449ed9999992c8f8e140786d943fccfe14d8bda
Pablo Rodriguez <103795541+RodriguezPabl@users.noreply.github.com>
2025-09-16 18:31:39 -0300

    Dependencias en el pom.xml

--- redsocial/src/main/java/equipo124/redsocial/util/JwtUtil.java
+++ redsocial/src/main/java/equipo124/redsocial/util/JwtUtil.java
@@ -1,10 +1,10 @@
 package equipo124.redsocial.util;
 
-
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
 import java.security.Key;
 import java.util.Date;
 
